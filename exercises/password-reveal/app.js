c5a6f08aac192e69e5a9a325cfebb2bd4382aaea
Steele <shrevesa@mail.uc.edu>
2022-09-19 22:15:41 -0400

    Create constants and blank onclick event listener

--- exercises/password-reveal/app.js
+++ exercises/password-reveal/app.js
@@ -1,12 +1,13 @@
 // 🤖: create 2 constants with references to the password input and the reveal button from the DOM
-
+const passwordInput = document.getElementById("#password");
+const visibilityToggle = document.getElementById("#togglePassword");
 
 // 🤖: Create an Event Listener on the password reveal button
 // 🤖: The event listener should toggle the password field attribute between `password` and `text` to change the visibility of the password field content.
-
+visibilityToggle.onclick = function () {};
 
 // Provided for you - prevent form submit
 const form = document.querySelector("form");
-form.addEventListener('submit',function(e){
-	e.preventDefault();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 });
